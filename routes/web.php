3eebb09f7ad5759ca51914955772e14d3dd87d6c
aldiui <aldijaya280902@gmail.com>
2023-12-25 02:08:14 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/storage-link', function () {
     Artisan::call('storage:link');
